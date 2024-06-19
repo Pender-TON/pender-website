e85c6cf1f78e014ddff425485820d9c516d7efc2
Aleksei Matveev <support@wisart.app>
2024-06-19 18:32:34 +0100

    language redirect fix 2.0

--- api/language.js
+++ api/language.js
@@ -1,5 +1,7 @@
 export default (req, res) => {
   const language = req.headers["accept-language"];
+  console.log("Accept-Language:", language); // Добавьте эту строку для логирования
+
   if (language && language.includes("ru")) {
     res.writeHead(302, { Location: "/ru" });
   } else {
