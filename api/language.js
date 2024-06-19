e303d85864e9c449098184085382bb59ab5b63b4
Aleksei Matveev <support@wisart.app>
2024-06-19 18:49:48 +0100

    language redirect fix 7.0

--- api/language.js
+++ api/language.js
@@ -1,21 +1,29 @@
 export default (req, res) => {
   const languageHeader = req.headers["accept-language"];
   console.log("Received Language Header:", languageHeader);
-  console.log("Preferred Language:", preferredLanguage);
-  console.log("Current URL:", req.url);
 
   // Парсим заголовок и ищем язык с наивысшим приоритетом
-  const preferredLanguage = languageHeader
-    .split(",")
-    .map((lang) => lang.split(";")[0].trim()) // Убираем качественный фактор, если он есть
-    .find((lang) => lang.includes("en") || lang.includes("ru")); // Ищем английский или русский
+  const languages = languageHeader.split(",");
+  const preferredLanguage = languages
+    .map((lang) => {
+      const [code, priority] = lang.split(";")[0].trim().split(";");
+      return {
+        code,
+        priority: priority ? parseFloat(priority.split("=")[1]) : 1,
+      };
+    })
+    .sort((a, b) => b.priority - a.priority)[0].code; // Получаем язык с наивысшим приоритетом
 
-  if (preferredLanguage && preferredLanguage.includes("ru")) {
+  console.log("Preferred Language:", preferredLanguage);
+
+  // Перенаправляем на нужную версию сайта на основе языка
+  if (preferredLanguage.includes("ru")) {
     console.log("Redirecting to Russian version");
     res.writeHead(302, { Location: "/ru" });
+    res.end();
   } else {
     console.log("Redirecting to English version");
     res.writeHead(302, { Location: "/" });
+    res.end();
   }
-  res.end();
 };
