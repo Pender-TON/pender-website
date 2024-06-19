ba862ead7fbded3abbcc8f005936d57d1c482467
Aleksei Matveev <support@wisart.app>
2024-06-19 18:56:29 +0100

    language redirect fix 8.0

--- api/language.js
+++ api/language.js
@@ -2,7 +2,6 @@ export default (req, res) => {
   const languageHeader = req.headers["accept-language"];
   console.log("Received Language Header:", languageHeader);
 
-  // Парсим заголовок и ищем язык с наивысшим приоритетом
   const languages = languageHeader.split(",");
   const preferredLanguage = languages
     .map((lang) => {
@@ -16,14 +15,20 @@ export default (req, res) => {
 
   console.log("Preferred Language:", preferredLanguage);
 
+  // Извлекаем текущий путь из запроса
+  const currentPath = new URL(req.url, `https://${req.headers.host}`).pathname;
+
   // Перенаправляем на нужную версию сайта на основе языка
-  if (preferredLanguage.includes("ru")) {
+  if (preferredLanguage.includes("ru") && currentPath !== "/ru") {
     console.log("Redirecting to Russian version");
     res.writeHead(302, { Location: "/ru" });
     res.end();
-  } else {
+  } else if (!preferredLanguage.includes("ru") && currentPath !== "/") {
     console.log("Redirecting to English version");
     res.writeHead(302, { Location: "/" });
+    res.end();
+  } else {
+    // Если текущий путь уже совпадает с требуемым, ничего не делаем
     res.end();
   }
 };
